b60f68e30a34cf1fec7f334650cb744ebf43d644
mart337i <mart337i@gamil.com>
2023-11-23 14:33:33 +0100

    [WIP]

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,10 +14,25 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
 
+    
+
     final LatLng _center = const LatLng(45.521563, -122.677433);
 
     void _onMapCreated(GoogleMapController controller) {
       mapController = controller;
+    }
+
+    Future<Position> _getCurrent_location() async {
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return Future.error("location service disabled");
+      }
+
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied){
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied)
+      }
     }
 
     @override
